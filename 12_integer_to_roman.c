67510a32d8905dd451ad700e77a3c2aa9dd74d3d
evan lau <814364672@qq.com>
2015-11-29 15:08:16 +0800

    Revert "Revert "#12, #14 commit""
    This reverts commit 339ffb15ddc6cbb7a6df270acd61de1d1d86a8bb.

---
+++ 12_integer_to_roman.c
@@ -0,0 +1,33 @@
+/**
+ * Description : Integer to Roman
+ *               Given an integer, convert it to a roman numeral.
+ *               Input is guaranteed to be within the range from 1 to 3999.
+ * Author      : Evan Lau
+ * Date        : 2015.11.01
+ */
+
+#include <stdio.h>
+
+char* intToRoman(int num)
+{
+    char bit_cnt = 0;
+    int tmp;
+    
+    while (num > 0)
+    {
+        tmp = num % 10;
+        switch (tmp)
+        {
+            
+        }
+    }
+}
+
+int main()
+{
+    
+    
+    return 0;
+}
+
+
