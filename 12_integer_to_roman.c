850695fefdcc5444eedeb6327cd850994fcc8934
lolosssss <814364672@qq.com>
2016-02-25 20:15:13 +0800

    #12 commit

--- 12_integer_to_roman.c
+++ 12_integer_to_roman.c
@@ -7,26 +7,56 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 char* intToRoman(int num)
 {
-    char bit_cnt = 0;
-    int tmp;
-    
+    int values[] = {
+        1000, 900, 500, 400,
+        100, 90, 50, 40,
+        10, 9, 5, 4,
+        1
+    };
+
+    char symbols[13][13] = {
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV",
+        "I"
+    };
+
+    char *ret = (char *)malloc(sizeof(char) * 32);
+    int i = 0;
+    int j = 0;
+    int cur = 0;
+    int len = 0;
+
+    memset(ret, 0, sizeof(char) * 32);
+
     while (num > 0)
     {
-        tmp = num % 10;
-        switch (tmp)
+        int k = num / values[i];
+        for (j = 0; j < k; j++)
         {
-            
+            len = strlen(symbols[i]);
+            strncat(ret + cur, symbols[i], len);
+            cur += len;
+            num -= values[i];
         }
+        i++;
     }
+
+    return ret;
 }
 
 int main()
 {
-    
-    
+    int n;
+
+    scanf("%d", &n);
+    printf("%s\n", intToRoman(n));
+
     return 0;
 }
 
