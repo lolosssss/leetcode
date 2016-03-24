176a7d56dc5288fbb18b8ad8f6cf0b4dd94b3732
lolosssss <814364672@qq.com>
2016-03-24 22:37:47 +0800

    #38 commit

---
+++ 38_count_and_say.c
@@ -0,0 +1,79 @@
+/**
+ * Description : Count and Say
+ *               The count-and-say sequence is the sequence of integers begining
+ *               as follows: 1, 11, 21, 1211, 111221, ...
+ *               1 is read off as "one 1" or 11.
+ *               11 is read off as "two 1" or 21.
+ *               21 is read off as "one 2, then one 1" or 1211.
+ *               Given an integer n, generate the nth sequence.
+ * Author      : Evan Lau
+ * Date        : 2016/03/22
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* countAndSay(int n)
+{
+    char *ret = (char *)malloc(sizeof(char) * 1024);
+    char buffer[1024];
+    char last;
+    int cnt = 0;
+    int i = 0;
+    int j = 0;
+
+    buffer[0] = '1';
+    buffer[1] = '\0';
+
+    if (n == 1)
+    {
+        ret[0] = '1';
+        ret[1] = '\0';
+
+        return ret;
+    }
+    n--;
+
+    while (n--)
+    {
+        cnt = 0;
+        j = 0;
+        i = 0;
+        last = buffer[0];
+        while (buffer[i] != '\0')
+        {
+            if (buffer[i] == last)
+            {
+                cnt++;
+            }
+            else
+            {
+                ret[j++] = cnt + '0';
+                ret[j++] = last;
+                last = buffer[i];
+                cnt = 1;
+            }
+            i++;
+        }
+        ret[j++] = cnt + '0';
+        ret[j++] = last;
+        ret[j] = '\0';
+        strcpy(buffer, ret);
+    }
+
+    return ret;
+}
+
+int main(void)
+{
+    char *res = NULL;
+    for (int i = 1; i < 21; i++)
+    {
+        res = countAndSay(i);
+        printf("%d : %s\n", i, res);
+        free(res);
+    }
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -38,6 +38,7 @@
 34 . search for a range  
 35 . search insert position  
 36 . valid Sudoku  
+38 . count and say
 
 ***  
 
